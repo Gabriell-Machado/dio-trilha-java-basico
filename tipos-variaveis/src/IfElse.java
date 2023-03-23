1b6f6ff92e04abb017b2b1ceb9ea4074a1ae1b98
Gabriell-Machado <lima.gabriel0429@gmail.com>
2023-03-23 09:31:14 -0300

    novo commit

---
+++ tipos-variaveis/src/IfElse.java
@@ -0,0 +1,22 @@
+public class IfElse {
+    public static void main(String[] args)  {
+        int a,b;
+        a = 5;
+        b = 5;
+
+        String resultado;
+        if(a==b)
+            resultado = "verdadeiro";
+        else 
+            resultado = "falso";
+
+        System.out.println(resultado);
+// or
+
+//        String resultado = a==b ? "verdadeiro" : "falso";
+
+//        System.out.println(resultado);
+
+    }
+}
+
